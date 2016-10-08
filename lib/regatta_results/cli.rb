9ea6933e2632a0bcfed283bb09ac5df10a61c112
Christina Cole <christinal.cole@gmail.com>
2016-10-08 22:31:56 +0000

    creating stub data for race list

--- lib/regatta_results/cli.rb
+++ lib/regatta_results/cli.rb
@@ -1,7 +1,18 @@
 class RegattaResults::CLI
 
   def call
-    puts "test statement for 2016 regattas"
+    puts "test statement for 2016 regattas:"
+    list_races
+  end
+
+  def list_races
+    puts <<-doc
+    1. Jan. 1	Hangover Bowl
+    2. Jan 31 - Mar 20	Frostbite - Second Half
+    3. April 23	Spring One Design Regatta
+    4. April 27 - June 1	WNR Series 1
+    5. May 14	Spring Harbor Regatta
+    doc
   end
   
 end
