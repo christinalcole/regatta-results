2ea318a70e195f78095f8214e5492833071a7957
Christina Cole <christinal.cole@gmail.com>
2016-10-10 22:35:20 +0000

    adding partial division_finish method

--- lib/regatta_results/cli.rb
+++ lib/regatta_results/cli.rb
@@ -4,7 +4,7 @@ class RegattaResults::CLI
     puts "test statement for 2016 regattas:"
     list_races
     division_menu
-    #pick division/handicap for finish times, then list division/handicap finish times
+    #division_finish
   end
 
   def list_races
@@ -26,6 +26,7 @@ class RegattaResults::CLI
       case input
       when "1"
         puts "There were 99 entries: Cal 25 - Harbor 20 - J/105 - J/22 - J/70 - J/80 - PHRF 1 - PHRF 2 - PHRF 3"
+        division_finish
       when "2"
         puts "there were 96 entries: Cal 25 - Harbor 20 - J/105 - J/22 - J/70 - J/80 - PHRF 1 - PHRF 2 - PHRF 3"
       when "3"
@@ -40,6 +41,20 @@ class RegattaResults::CLI
   end
 
   def division_finish
+    finish_input = nil
+    while finish_input != "exit"
+      puts "Select a series (e.g., 1, 2, 3, 4, 5) for finish order, finish time, correction, etc.:"
+      finish_input = gets.strip
+      
+      case finish_input
+      when "PHRF 1"
+        puts "here's the PHRF 1 results"
+      when "PHRF 2"
+        puts "here's the PHRF 2 results"
+      when "PHRF 3"
+        puts "here's the PHRF 3 results"
+      end
+    end
   end
 
   # each series has: name, division(s), date/time scored, number_of_races scored, overall_standing, url, boat_list
