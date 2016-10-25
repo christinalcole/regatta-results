0805d99a63e382228c68a0f3cfaabd8a0143fee9
Christina Cole <christinal.cole@gmail.com>
2016-10-25 20:56:27 +0000

    adding stub of general boat object model

--- lib/regatta_results/boat.rb
+++ lib/regatta_results/boat.rb
@@ -1,5 +1,23 @@
 class RegattaResults::Boat
 
   attr_accessor :name, :sail_number, :skipper, :series_standing, :series_finishes, :series_points
-  
+
+  #general model for a boat object
+
+  boat_1 = self.new
+  boat_1.name = "Xiberger"
+  boat_1.sail_number = "52038"
+  boat_1.skipper = "White/Mangold, William/Harald"
+  boat_1.series_standing = "1"
+  boat_1.series_finishes = {race_1: 1, race_2: [4], race_3: 1, race_4: 1, race_5: 1}
+  boat_1.series_points = "4" #points equal sum of series_finishes values, less tossed race []
+
+  boat_2 = self.new
+  boat_2.name = "Wanda"
+  boat_2.sail_number = "93239"
+  boat_2.skipper = "Jensen, Christian"
+  boat_2.series_standing = "1"
+  boat_2.series_finishes = {race_1: 2, race_2: [7], race_3: 2, race_4: 3, race_5: 3}
+  boat_2.series_points = "10"
+
 end
