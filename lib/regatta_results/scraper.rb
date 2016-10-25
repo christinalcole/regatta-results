2e9984c57b584e91c1ddfb1f88bb04b48fc1a6c5
Christina Cole <christinal.cole@gmail.com>
2016-10-25 22:53:42 +0000

    initial commit

---
+++ lib/regatta_results/scraper.rb
@@ -0,0 +1,10 @@
+class RegattaResults::Scraper
+
+  # objectives:
+  # 1. scrape WNR URL (one URL per series)
+  # 2. Instantiate a Regatta object for each series
+  # 3. Scrape the boats that raced in that series
+  # 4. Scrape individual boats/instantiate Boat objects for that series, add them to the Regatta instance
+
+  
+end
