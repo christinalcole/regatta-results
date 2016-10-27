0dd06de63519750e88cc0fbd584d3e1e526e8b68
Christina Cole <christinal.cole@gmail.com>
2016-10-27 03:14:53 +0000

    updating to require nokogiri, pry

--- lib/regatta_results.rb
+++ lib/regatta_results.rb
@@ -4,3 +4,8 @@ require_relative "./regatta_results/version"
 require_relative "./regatta_results/cli"
 require_relative "./regatta_results/regatta"
 require_relative "./regatta_results/boat"
+require_relative "./regatta_results/scraper"
+
+require 'nokogiri'
+require 'open-uri'
+require 'pry'
