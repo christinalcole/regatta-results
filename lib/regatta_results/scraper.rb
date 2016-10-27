3e95715194d003fe68a2e5bae32a33d95c8318a0
Christina Cole <christinal.cole@gmail.com>
2016-10-27 03:15:54 +0000

    preliminary notes, paths for data scraping

--- lib/regatta_results/scraper.rb
+++ lib/regatta_results/scraper.rb
@@ -6,5 +6,26 @@ class RegattaResults::Scraper
   # 3. Scrape the boats that raced in that series
   # 4. Scrape individual boats/instantiate Boat objects for that series, add them to the Regatta instance
 
-  
+  def self.scrape_boat_list
+    boat_list = []
+    #student_hash = {}
+
+    list_html = open("http://www.annapolisyc.com/files/2016WNRSeries1.html")
+
+    doc = Nokogiri::HTML(list_html)
+    binding.pry
+
+
+# division name: doc.css("p.divisionheader a").attribute("name").children[0].text
+# sail number: doc.css("tbody tr").children[3].text
+# boat name: doc.css("tbody tr").children[5].text  ** NEED THIS FOR LIST VIEW **
+# skipper: doc.css("tbody tr").children[7].text
+# series_standing: doc.css("tbody tr").children[1].text
+# series_finishes: race 1, " .children[9].text  race 2, ".children[11].text, 3 = [13], 4 = [15], 5 = [17]
+# series_points: doc.css("tbody tr".children[19].text
+
+# race notes: doc.css("table.footnotes td")[5..-1].children[0].text, children[2].text (first 2 notes)
+
+
+  end
 end
