7c1dc78726876a98585f48c267daca6bbb9fa5de
Christina Cole <christinal.cole@gmail.com>
2016-10-29 20:06:39 +0000

    format clean-up

--- lib/regatta_results/cli.rb
+++ lib/regatta_results/cli.rb
@@ -16,60 +16,44 @@ class RegattaResults::CLI
 
       case input
       when "1"
-
         make_boats("http://www.annapolisyc.com/files/2016WNRSeries1.html")
         detailed_view
         input = nil
         until input == "n"
+          puts "\n\nWould you like to see details for another boat in this series? (y/n)"
+          input = gets.strip.downcase
 
-        puts "\n\nWould you like to see details for another boat in this series? (y/n)"
-
-        input = gets.strip.downcase
-
-        if input == "y"
-          detailed_view
+          if input == "y"
+            detailed_view
+          end
         end
-      end
-        # puts "#{RegattaResults::Boat.all.length} boats raced in Series 1 in the following handicaps:\n 1. Alberg 30 (6 boats) \t\t 8. J/35 (5 boats)\n 2. Etchells (15) \t\t\t 9. J/70 (7)\n 3. Farr 30 (5) \t\t\t 10.J/80 (8)\n 4. Harbor 20 (20) \t\t\t 11.PHRF 0 (11)\n 5. Herrschoff 12.5 (6) \t\t 12.PHRF 1 (9)\n 6. J/105 (16) \t\t\t\t 13.PHRF 2 (4)\n 7. J/30 (10) \t\t\t\t 14.PHRF 3 (10)\n\nEnter the number of the handicap of interest:"
-        #
-        # range = gets.strip.to_i
-        # display_boats(range)
-        #
-        # puts "\nEnter the row number of the boat for which you'd like to see more information"
-        # input = gets.strip.to_i
-        # display_the_boat(input)
       when "2"
         RegattaResults::Boat.empty
         make_boats("http://www.annapolisyc.com/files/2016WNRSeries2_751.html")
         detailed_view
         input = nil
         until input == "n"
+          puts "\n\nWould you like to see details for another boat in this series? (y/n)"
+          input = gets.strip.downcase
 
-        puts "\n\nWould you like to see details for another boat in this series? (y/n)"
-
-        input = gets.strip.downcase
-
-        if input == "y"
-          detailed_view
+          if input == "y"
+            detailed_view
+          end
         end
-      end
-
       when "3"
         RegattaResults::Boat.empty
         make_boats("http://www.annapolisyc.com/files/2016WNRSeries3_751.html")
         detailed_view_series_3
         input = nil
         until input == "n"
+          puts "\n\nWould you like to see details for another boat in this series? (y/n)"
 
-        puts "\n\nWould you like to see details for another boat in this series? (y/n)"
+          input = gets.strip.downcase
 
-        input = gets.strip.downcase
-
-        if input == "y"
-          detailed_view_series_3
+          if input == "y"
+            detailed_view_series_3
+          end
         end
-      end
-
       when "list"
         list_series
       when "exit"
