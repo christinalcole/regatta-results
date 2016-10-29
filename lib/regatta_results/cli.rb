1cf64ed6b932d044065f5638c1d7ec9c49792ff0
Christina Cole <christinal.cole@gmail.com>
2016-10-29 20:03:48 +0000

    customizing detailed view methods based on series data

--- lib/regatta_results/cli.rb
+++ lib/regatta_results/cli.rb
@@ -57,7 +57,7 @@ class RegattaResults::CLI
       when "3"
         RegattaResults::Boat.empty
         make_boats("http://www.annapolisyc.com/files/2016WNRSeries3_751.html")
-        detailed_view
+        detailed_view_series_3
         input = nil
         until input == "n"
 
@@ -66,7 +66,7 @@ class RegattaResults::CLI
         input = gets.strip.downcase
 
         if input == "y"
-          detailed_view
+          detailed_view_series_3
         end
       end
 
@@ -154,6 +154,67 @@ class RegattaResults::CLI
     end
   end
 
+  def display_boats_series_3(range)
+    case range
+    when 1
+      RegattaResults::Boat.all[0,6].each.with_index(1) do |boat, index|
+        puts "#{index}. #{boat.sail_number} - #{boat.name}"
+      end
+    when 2
+      RegattaResults::Boat.all[6,15].each.with_index(7) do |boat, index|
+        puts "#{index}. #{boat.sail_number} - #{boat.name}"
+      end
+    when 3
+      RegattaResults::Boat.all[21,5].each.with_index(22) do |boat, index|
+        puts "#{index}. #{boat.sail_number} - #{boat.name}"
+      end
+    when 4
+      RegattaResults::Boat.all[26,20].each.with_index(27) do |boat, index|
+        puts "#{index}. #{boat.sail_number} - #{boat.name}"
+      end
+    when 5
+      RegattaResults::Boat.all[46,6].each.with_index(47) do |boat, index|
+        puts "#{index}. #{boat.sail_number} - #{boat.name}"
+      end
+    when 6
+      RegattaResults::Boat.all[52,16].each.with_index(53) do |boat, index|
+        puts "#{index}. #{boat.sail_number} - #{boat.name}"
+      end
+    when 7
+      RegattaResults::Boat.all[68,11].each.with_index(69) do |boat, index|
+        puts "#{index}. #{boat.sail_number} - #{boat.name}"
+      end
+    when 8
+      RegattaResults::Boat.all[79,5].each.with_index(80) do |boat, index|
+        puts "#{index}. #{boat.sail_number} - #{boat.name}"
+      end
+    when 9
+      RegattaResults::Boat.all[84,7].each.with_index(85) do |boat, index|
+        puts "#{index}. #{boat.sail_number} - #{boat.name}"
+      end
+    when 10
+      RegattaResults::Boat.all[91,8].each.with_index(92) do |boat, index|
+        puts "#{index}. #{boat.sail_number} - #{boat.name}"
+      end
+    when 11
+      RegattaResults::Boat.all[99,12].each.with_index(100) do |boat, index|
+        puts "#{index}. #{boat.sail_number} - #{boat.name}"
+      end
+    when 12
+      RegattaResults::Boat.all[111,10].each.with_index(112) do |boat, index|
+        puts "#{index}. #{boat.sail_number} - #{boat.name}"
+      end
+    when 13
+      RegattaResults::Boat.all[121,4].each.with_index(122) do |boat, index|
+        puts "#{index}. #{boat.sail_number} - #{boat.name}"
+      end
+    when 14
+      RegattaResults::Boat.all[125,10].each.with_index(126) do |boat, index|
+        puts "#{index}. #{boat.sail_number} - #{boat.name}"
+      end
+    end
+  end
+
   def display_the_boat(index)
     boat = RegattaResults::Boat.all[index-1]
     puts "---------Series info for Sail Number #{boat.sail_number}---------"
@@ -171,10 +232,21 @@ class RegattaResults::CLI
   end
 
   def detailed_view
-    puts "#{RegattaResults::Boat.all.length} boats raced in Series 1 in the following handicaps:\n 1. Alberg 30 (6 boats) \t\t 8. J/35 (5 boats)\n 2. Etchells (15) \t\t\t 9. J/70 (7)\n 3. Farr 30 (5) \t\t\t 10.J/80 (8)\n 4. Harbor 20 (20) \t\t\t 11.PHRF 0 (11)\n 5. Herrschoff 12.5 (6) \t\t 12.PHRF 1 (9)\n 6. J/105 (16) \t\t\t\t 13.PHRF 2 (4)\n 7. J/30 (10) \t\t\t\t 14.PHRF 3 (10)\n\nEnter the number of the handicap of interest:"
+    puts "#{RegattaResults::Boat.all.length} boats raced in this series in the following handicaps:\n 1. Alberg 30 (6 boats) \t\t 8. J/35 (5 boats)\n 2. Etchells (15) \t\t\t 9. J/70 (7)\n 3. Farr 30 (5) \t\t\t 10.J/80 (8)\n 4. Harbor 20 (20) \t\t\t 11.PHRF 0 (11)\n 5. Herrschoff 12.5 (6) \t\t 12.PHRF 1 (9)\n 6. J/105 (16) \t\t\t\t 13.PHRF 2 (4)\n 7. J/30 (10) \t\t\t\t 14.PHRF 3 (10)\n\nEnter the number of the handicap of interest:"
 
     range = gets.strip.to_i
     display_boats(range)
+
+    puts "\nEnter the row number of the boat for which you'd like to see more information"
+    input = gets.strip.to_i
+    display_the_boat(input)
+  end
+
+  def detailed_view_series_3
+    puts "#{RegattaResults::Boat.all.length} boats raced in this series in the following handicaps:\n 1. Alberg 30 (6 boats) \t\t 8. J/35 (5 boats)\n 2. Etchells (15) \t\t\t 9. J/70 (7)\n 3. Farr 30 (5) \t\t\t 10.J/80 (8)\n 4. Harbor 20 (20) \t\t\t 11.PHRF 0 (12)\n 5. Herrschoff 12.5 (6) \t\t 12.PHRF 1 (10)\n 6. J/105 (16) \t\t\t\t 13.PHRF 2 (4)\n 7. J/30 (11) \t\t\t\t 14.PHRF 3 (10)\n\nEnter the number of the handicap of interest:"
+
+    range = gets.strip.to_i
+    display_boats_series_3(range)
 
     puts "\nEnter the row number of the boat for which you'd like to see more information"
     input = gets.strip.to_i
