571e12962265d4a60a417e3c341f2a488501aee6
Christina Cole <christinal.cole@gmail.com>
2016-10-31 23:29:48 +0000

    removing RegattaResults::Regatta dependency

--- lib/regatta_results.rb
+++ lib/regatta_results.rb
@@ -2,7 +2,6 @@ require_relative "./regatta_results/version"
 
 
 require_relative "./regatta_results/cli"
-require_relative "./regatta_results/regatta"
 require_relative "./regatta_results/boat"
 require_relative "./regatta_results/scraper"
 
